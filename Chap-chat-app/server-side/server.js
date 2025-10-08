2a0d2c9b31298b9b87f4d615e4d8a9b154772750
paultheachiever <paulkiruthumwangi@gmail.com>
2025-10-09 02:37:38 +0300

    rr

--- Chap-chat-app/server-side/server.js
+++ Chap-chat-app/server-side/server.js
@@ -19,9 +19,7 @@ const io = new Server(server, {
       "https://chap-chat-frontend.onrender.com", 
       "http://localhost:5173"                   
     ],
-    methods: ["GET", "POST"],
-},
-transports: ["websocket", "polling"],
+}
 });
 
 socketInit(io);
